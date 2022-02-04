b6d2e664aac82d91c5cb617cd002d6783477ee8b
MikhaZyu <MikhaZyu@gmail.com>
2022-02-04 13:33:15 +0300

    Refactoring

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Models/UpdateEmployeeDto.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Models/UpdateEmployeeDto.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Otus.Teaching.PromoCodeFactory.WebHost.Controllers
+namespace Otus.Teaching.PromoCodeFactory.WebHost.Models
 {
     public class UpdateEmployeeDto: CreateEmployeeDto
     {
